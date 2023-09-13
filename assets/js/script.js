fffc6bf6c364906dd65ecb748a231edfb5726519
Malte Mosbach <maltemosbach@gmail.com>
2023-09-13 13:58:45 +0200

    Initial commit.

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+let papertitle = "Grasp Anything: Augmenting Reinforcement Learning with Instance Segmentation to Grasp Arbitrary Objects"
+
+// set papertitle
+document.getElementById("papertitle_website").innerHTML = papertitle;
+document.getElementById("papertitle_heading").innerHTML = papertitle;
